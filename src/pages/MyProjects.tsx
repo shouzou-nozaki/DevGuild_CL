1d43796a986b8610596800737ff43f112ce89ecb
shouzou-nozaki <soro0530@icloud.com>
2025-01-16 20:38:23 +0900

    feat:DiscordBot招待処理の追加

--- src/pages/MyProjects.tsx
+++ src/pages/MyProjects.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./MyProjects.css";
 import { useUser } from "../util/UserContext";
 import { ProjectService } from "../services/ProjectService";
@@ -9,6 +9,7 @@ import { Const } from "../util/Const";
 const MyProjects = () => {
     const [projectList, setProjectList] = useState<Array<ProjectInfo>>([]); // プロジェクトリストの状態
     const { user } = useUser(); // ユーザー情報とセット関数を取得
+    const navigate = useNavigate(); // 遷移用フック
 
     useEffect(() => {
         fetchProjects();
@@ -28,6 +29,39 @@ const MyProjects = () => {
         } catch (err) {
             console.error(err);
         }
+    }
+
+    /**
+     * プロジェクト編集画面への遷移メソッド
+     * @param project 編集対象のプロジェクト情報
+     */
+    const toEditWindow = (project: ProjectInfo) => {
+        navigate(`/create?projectId=${project.ProjectId}`, {
+            state: { projectInfo: project, mode: Const.MODE_UPDATE_PROJECT },
+        });
+    }
+
+    /**
+     * DiscordServerID保持判定する関数
+     * @param project 
+     * @returns 
+     */
+    const hasDiscordServerID = (project: ProjectInfo): boolean => {
+        return project.DiscordServerId !== undefined && project.DiscordServerId !== null && project.DiscordServerId.trim() !== "";
+    }
+
+    /**
+     * DiscordBotへのサーバー招待メソッド
+     */
+    const inviteDiscordBotToServer = () => {
+        const clientId = process.env.REACT_APP_DISCORD_CLIENT_ID;
+        if (!clientId) {
+            console.error("環境変数が不足しています");
+            return;
+        }
+        // DiscordBOT招待URL
+        const botInviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot`;
+        window.open(botInviteUrl, '_blank');
     }
 
     return (
@@ -50,11 +84,13 @@ const MyProjects = () => {
                             <td>{project.Description}</td>
                             <td>{project.DueDate}</td>
                             {project.Status === "0" ? <td className="status_private">非公開</td> : <td className="status_public">公開</td>}
-                            
                             <td>
-                                <Link to={`/create?projectId=${project.ProjectId}`} state={{ projectInfo: project, mode: Const.MODE_UPDATE_PROJECT }} className="edit-button">
+                                <button onClick={() => toEditWindow(project)} className="edit-button">
                                     編集
-                                </Link>
+                                </button>
+                                <button className="public_button" disabled={!hasDiscordServerID(project)} onClick={inviteDiscordBotToServer}>
+                                    公開
+                                </button>
                             </td>
                         </tr>
                     ))}
